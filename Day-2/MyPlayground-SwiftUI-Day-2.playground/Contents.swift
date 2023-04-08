83f4cb1a2d1252618ca2c19cf5dcf6fee75a0921
Joabe Santana <joabesantana.c@gmail.com>
2023-04-08 01:20:52 -0300

    Day two

---
+++ Day-2/MyPlayground-SwiftUI-Day-2.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+
+let verdadeiro = true
+var falso = false
+falso.toggle()
+print(falso)
+falso.toggle()
+print(falso)
+
+print(falso)
+print(!falso)
+print(falso)
+falso.toggle() //change de value
+print(falso)
+falso.toggle() //change de value
+print(falso)
+
+
+var name = "Jobs"
+var lastName = "Santana Correia"
+//print(name + " Santana " + lastName)
+
+print("Hello \(name.uppercased()) seu sobrenome é: \"\(lastName)\"")
+
+let idade = 29
+
+print("Jobs tem \(idade) anos")
+print("\(name) vai fazer \(idade + 1) anos em Dezembro")
